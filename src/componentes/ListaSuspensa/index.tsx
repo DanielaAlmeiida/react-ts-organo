554f79e6733cd90e46bbfd6fcf79770251004df9
Daniela Almeida <danielaspbrasil@gmail.com>
2024-05-15 11:35:31 -0300

    Refatorando o Formulário

--- src/componentes/ListaSuspensa/index.tsx
+++ src/componentes/ListaSuspensa/index.tsx
@@ -4,15 +4,15 @@ interface ListaSuspensaProps {
     aoAlterado: (valor: string) => void,
     label: string,
     valor: string,
-    required: boolean,
+    obrigatorio: boolean,
     itens: string[]
 }
 
-const ListaSuspensa = ({ aoAlterado, label, valor, required, itens} : ListaSuspensaProps) => {
+const ListaSuspensa = ({ aoAlterado, label, valor, obrigatorio, itens} : ListaSuspensaProps) => {
     return (
         <div className='lista-suspensa'>
             <label>{label}</label>
-            <select onChange={evento =>aoAlterado(evento.target.value)} required={required} value={valor}>
+            <select onChange={evento =>aoAlterado(evento.target.value)} required={obrigatorio} value={valor}>
                 <option value=""></option>
                 {itens.map(item => {
                     return <option key={item}>{item}</option>
